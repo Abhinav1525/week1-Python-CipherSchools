b4a539f92eae86c13356b7acdbf42aa0d85f50df
Abhinav Bansal <118711459+Abhinav1525@users.noreply.github.com>
2022-11-27 12:45:27 +0530

    Add files via upload
    This is my python repository of learning from cipher schools.

---
+++ class10-escape sequences-CipherSchools.py.py
@@ -0,0 +1,3 @@
+print(' I\'m Abhinav Bansal ')
+print('Be it \\ dfg')
+print("git bash \"")
---
+++ class18-Variable in Python-CipherSchools.py.py
@@ -0,0 +1,16 @@
+number1=2
+print(numberl)
+number1 = 4
+print(number1)
+string, number
+name = "aman"
+print(name)
+name= 123
+print(name)
+#rule one
+#1number=4
+#letter ..................... first_letter
+_name= "aman"
+#ulsd = 34
+user_one_name = "Harry" # snake case writing
+userOneName = "Mohit" # camel case writing
